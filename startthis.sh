cc18e1e84f900a60efb1593e4f5d3803dc86106d
Aldrin Jenson <aldrinjenson@gmail.com>
2023-07-11 17:01:06 +0530

    add start script

---
+++ startthis.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# one click start script
+
+if ! ss -ln | grep -q ':7860'; then
+    echo "No service running on port 7860. Please make sure that stable-diffusion-web-ui is running on port 7860 and try again."
+    exit 1
+fi
+
+
+docker compose up --build -d
