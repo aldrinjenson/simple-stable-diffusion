fc8852da2bb88fff9be1e3c8856712ab7d5d5144
Aldrin Jenson <aldrinjenson@gmail.com>
2023-07-21 19:22:27 +0530

    Fix issue with file download

--- src/misc_utils.py
+++ src/misc_utils.py
@@ -8,14 +8,13 @@ import json
 def getDownloadHref(base64_image_url):
     random_number = random.randint(1000, 9999)
     filename = f"image{random_number}.png"
-    href = f'<a href="data:application/octet-stream;base64,{base64_image_url}" download="{filename}" style="text-decoration: none; padding: 8px 16px; background-color: transparent; color: #eee; border: 1px solid #333; border-radius: 4px;">Save image ⬇</a>'
+    href = f'<a href="{base64_image_url}" download="{filename}" style="text-decoration: none; padding: 8px 16px; background-color: transparent; color: #eee; border: 1px solid #333; border-radius: 4px;">Save image ⬇</a>'
     return href
 
 
 quotes = []
 file_path = "src/quotes-1000.json"
 with open(file_path, "r") as file:
-    print("opening quotes")
     quotes = json.load(file)
 
 
